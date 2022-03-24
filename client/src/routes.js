72547cc58a1d607b5ceaed0fd407745a725e1004
Aoino181 <smirnovroman181@gmail.com>
2022-03-24 17:03:53 +0300

    client: Настройка роутов react-router-dom

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,6 +1,10 @@
 import Admin from "./pages/Admin"
 import Basket from "./pages/Basket"
-import { ADMIN_ROUTE, BASKET_ROUTE } from "./utils/consts"
+import Shop from "./pages/Shop"
+import Auth from "./pages/Auth"
+import DevicePage from './pages/DevicePage'
+
+import { ADMIN_ROUTE, BASKET_ROUTE, DEVICE_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from "./utils/consts"
 
 export const authRoutes = [
     {
@@ -14,5 +18,20 @@ export const authRoutes = [
 ]
 
 export const publicRoutes = [
-
+    {
+        path: SHOP_ROUTE,
+        Component: Shop 
+    },
+    {
+        path: LOGIN_ROUTE,
+        Component: Auth
+    },
+    {
+        path: REGISTRATION_ROUTE,
+        Component: Auth
+    },
+    {
+        path: DEVICE_ROUTE + '/:id',
+        Component: DevicePage
+    },
 ]
